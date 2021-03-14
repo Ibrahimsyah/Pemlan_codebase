9c25b40bb8cd770bb86b9d5f3b53dd7bb4a76c36
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-14 16:05:19 +0700

    add problem1 overload

---
+++ overload/Problem1.java
@@ -0,0 +1,24 @@
+package overload;
+
+/**
+ *
+ * @author ibrahimsyah
+ */
+public class Problem1 {
+//    Terdapat suatu program yang dapat menghitung sebuah bangun ruang. program tersebut 
+//    dapat menerima input dan menghasilkan suatu hasil berupa penghitungan volume bangun ruang tersebut. 
+//    Bangun ruang tersebut memiliki atribut panjang, lebar, dan tinggi (semuanya dapat menampung bilangan 
+//    desimal lebih dari nol. Program itu sangat fleksibel karena pengguna dapat memberikan input 
+//    sesuai keinginan mereka ketika bangun ruang tersebut dibuat
+//    1. Jika input tidak diberikan sama sekali, maka keseluruhan atribut akan bernilai 0
+//    2. Jika input berjumlah 1, maka input tersebut akan menjadi panjang, lebar, dan tinggi
+//    3. Jika input berjumlah 2 (katakanlah bil1 dan bil2), maka bil1 akan menjadi panjang dan lebar, 
+//       dan bil3 menjadi tinggi
+//    3. Jika input merupakan array angka desimal dengan panjang 3, maka array[0] menjadi panjang, 
+//       array[1] menjadi lebar, dan array[2] menjadi tinggi
+//    Program tersebut diciptakan untuk dapat menghitung volume
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
