be50964c7bf784ad1e1f233e5549e03a0483edd2
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-15 07:10:04 +0700

    add author

--- overload/Problem3.java
+++ overload/Problem3.java
@@ -1,5 +1,20 @@
 package overload;
 
 public class Problem3 {
-    
+//    Sebuah program diciptakan untuk melakukan simulasi pengujian mobil. 
+//    Setiap mobil memiliki panjang, berat, dan kecepatan 
+//    pengujian dapat dilakukan dengan menggunakan 
+//    fungsi yang ada pada mobil tersebut bernama ujiBalap. 
+//    ujiBalap memiliki beberapa fungsi tergantung dari input yang 
+//    diberikan kepadanya:
+//    1. Jika input berupa jarak (int), maka akan mengembalikan nilai 
+//    desimal yang bernilai jarak / kecepatan
+//    2. Jika input berupa jumlahBBM (double), maka akan mengembalikan 
+//    nilai desimal berupa berat / jumlahBBM
+//    3. Jika input berupa jarak (int) dan jumlahBBM (double), 
+//    maka akan mencetak "Mobil melaju dengan kecepatan <kecepatan> dan dengan efisiensi bahan bakar <efisiensi>"
+//    dengan efiensi = jumlahBBM / jarak
+//    4. Jika input berupa jumlahBBM (double) dan beratTambahan (int), 
+//    maka akan mengembalikan nilai desimal 
+//    berupa (berat + beratTambahan)/jumlahBBM
 }
--- overload/Problem4.java
+++ overload/Problem4.java
@@ -5,5 +5,13 @@ package overload;
  * @author ibrahimsyah
  */
 public class Problem4 {
-    
+//    Terdapat sebuah generator string yang akan membantu kita dalam membuat string dengan mudah. 
+//    Awalnya generator tersebut meminta nama kita dan generator tersebut memiliki 1 fungsi 
+//    bernama generate yang memiliki banyak fitur diantaranya:
+//    1. Jika generate dipanggil tanpa diberi parameter, maka akan mengembalikan 
+//       String berisi "Halo <nama>!"
+//    2. Jika dipanggil dengan 1 input umur (int), maka akan mengembalikan String 
+//       berisi "Halo <nama>, kamu berumur <umur> tahun!"
+//    3. Jika dipanggil dengan 1 input namaBelakang (string), maka akan mengembalikan 
+//       String berisi "Halo <nama> <namaBelakang>!"
 }
