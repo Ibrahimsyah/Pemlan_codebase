8d6828c4546a9d677879f424d872dbe9a377a1c3
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-15 08:55:58 +0700

    revise problem5

--- overload/Problem5.java
+++ overload/Problem5.java
@@ -13,7 +13,7 @@ public class Problem5 {
 //
 //    3. Sebaliknya, ketika hanya diisi 1 input desimal maka hanya bil2 yang terisi dan bil1 menjadi 10 
 //
-//    4. Ketika diisi 2 inputan bilangan bulat dan desimal, maka 2 input itu akan masuk ke bilangan tersebut 
+//    4. Ketika diisi 2 inputan bilangan bulat dan desimal, maka 2 input itu akan masuk ke bil1 dan bil2. 
 //
 //    5. Ketika diisi 3 inputan bilangan dengan urutan input1 bulat, input2 bulat, input3 pecahan. 
 //       Maka bil1 akan berisi input1 + input2 dan bil2 akan berisi input3 
