fdf97a3a050c900e17f1f4e813532c0d2313f679
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-22 08:38:00 +0700

    add jamboard link

--- encapsulation/Main.java
+++ encapsulation/Main.java
@@ -12,6 +12,9 @@ import encapsulation.hewan.Kucing;
  * @author ibrahimsyah
  */
 public class Main {
+    
+    // Jamboard
+    // https://jamboard.google.com/d/1badB08Q3_aTBb-EgTSmnMkevjBUPyI1FEG2mPXFSXYg/edit?usp=sharing
     public static void main(String[] args) {
         Generator generator = new Generator();
         Kucing kucing1 = generator.buatKucing("Marco", 10);
