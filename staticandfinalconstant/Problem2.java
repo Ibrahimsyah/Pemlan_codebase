e79864dd8676cc5ae4868aec89e314efc25806dd
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-03-28 22:29:42 +0700

    add problem2

--- staticandfinalconstant/Problem2.java
+++ staticandfinalconstant/Problem2.java
@@ -11,4 +11,14 @@ package staticandfinalconstant;
  */
 public class Problem2 {
     
+//    Suatu kampus bernama BU berniat untuk membuat program pendata mahasiswa baru.
+//    mahasiswa yang akan di daftarkan akan memiliki nim, nama, dan umur.
+//    pada saat pembuatannya, hanya nama dan umur saja yang diberikan karena nim akan
+//    di buatkan otomatis oleh sistem dan nim setiap siswa berbeda satu sama lain.
+//    penentuan nim dilakukan dengan aturan:
+//	a. nim pertama bernilai 29032020101
+//	b. nim kedua bernilai 29032020105
+//	c. nim ketiga bernilai 29032020109
+//    Kampus tersebut dapat melakukan cetakDataMahasiswa dengan menginputkan mahasiswa, 
+//    dan akan mengeluarkan output berupa data diri mahasiswa tersebut.
 }
