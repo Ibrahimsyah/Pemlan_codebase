c0a116c5cf51115548b77d86d68fa31e26a581b2
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-04-19 08:11:57 +0700

    fix problem1 had not saved

--- objectreference/Problem1.java
+++ objectreference/Problem1.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package objectreference;
 
 /**
  *
  * @author ibrahimsyah
  */
+
 public class Problem1 {
-    
+//    Terdapat sebuah sistem yang memiliki kemampuan untuk menyimpan mobil.
+//    Mobil memiliki atribut berupa mesin, roda yang berjumlah 4, bahan bakar, tipe
+//    (Sedan, sport, SUV), berat, kapasitas tangki.
+//    Mesin memiliki atribut berupa berat dan konsumsiPerKm
+//    Bahan bakar memiliki atribut oktan dan hargaPerLiter
+//    Mobil dapat mengganti rodanya dengan menggunakan fungsi gantiRoda
+//    yang menerima parameter sebuah objek roda dan n yang merupakan roda keberapa
+//    yang akan diganti. Kemudian mobil memiliki fungsi untuk isiBahanBakar yang menerima 
+//    parameter objek BBM dan jumlah dengan mengembalikan nilai berupa berapa banyak 
+//    biaya yang di keluarkan. Mobil hanya dapat menerima bahan bakar
+//    yang memiliki oktan yang sama. Mobil tentunya memiliki kemampuan untuk jalan 
+//    dengan jarak tertentu dan akan menghasilkan nilai berapa banyak bahan bakar yang habis.
+//    terdapat fungsi pada class Mobil bernama tuneUp yang digunakan untuk mengganti 
+//    mesin mobil dan fungsi ukurBerat yang menerima parameter objek mobil dan 
+//    mengembalikan nilai berupa berat total mobil tersebut
 }
