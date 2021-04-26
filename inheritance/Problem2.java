8c0192866b0d13dd7c2c4a0f187240c44bba861e
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-04-26 12:06:06 +0700

    add problem 2

--- inheritance/Problem2.java
+++ inheritance/Problem2.java
@@ -10,5 +10,20 @@ package inheritance;
  * @author ibrahimsyah
  */
 public class Problem2 {
-    
+//        Di suatu perusahaan, terdapat Pegawai yang selalu bekerja. Pegawai memiliki
+//    atribut NIP, nama, tahunMasuk, dan kelamin. Pekerja memiliki kemampuan untuk
+//    bekerja yang mana ketika di panggil akan mencetak "Pekerja sedang bekerja".
+//    Pekerja juga memiliki kemampuan untuk istirahat yang akan mencetak "Pekerja
+//    sedang istirahat"
+//
+//    Pegawai dibagi menjadi beberapa role spesifik yaitu:
+//    1. Sekretaris: memiliki atribut tambahan yaitu kecepatanTulis. 
+//       Kemampuan bekerja pada sekretaris akan mencetak 
+//       "Sekretaris mengurus administrasi dengan kecepatan ketik <kecepatanTulis> kpm". 
+//       Ketika istirahat, akan mencetak "Sekretaris sedang makan siang", dan ketika 
+//       istirahat dipanggil menggunakan 1 parameter bertipe String maka akan mencantumkan 
+//       makanan yang sedang di makan. Contoh: "Sekretaris sedang makan siang dengan Cabai"
+//
+//    2. Teknisi: memiliki atribut tambahan yaitu lamaKerja. Kemampuan bekerja pada 
+//       role ini akan mencetak "Teknisi <nama> sedang bekerja".
 }
