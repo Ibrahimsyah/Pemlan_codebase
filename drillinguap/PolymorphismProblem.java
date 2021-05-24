dba67b075e1c8a5c0f9d2a72d7205cf63e12d12d
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-05-24 07:06:06 +0700

    add drilling uap

---
+++ drillinguap/PolymorphismProblem.java
@@ -0,0 +1,12 @@
+package drillinguap;
+
+/**
+ *
+ * @author Ibrahimsyah
+ */
+
+public class PolymorphismProblem {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
